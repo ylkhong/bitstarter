29b3dea9732dcb1ddce540532a33cd514161ab3c
Ubuntu <ubuntu@ip-172-31-17-23.us-west-2.compute.internal>
2013-07-13 15:54:11 +0000

    Modified agian web.js

--- web.js
+++ web.js
@@ -2,14 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html', function(err,data){
-if (err) throw err;
-console.log(data);
-});
+var buffer = new Buffer(30);
 
-var buffer = new Buffer(data,"utf-8"); 
-
-
+var text = fs.readFileSync('index.html',);
+console.log(text);
+ 
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8');
 });
