f4350b358329cb12c50fd170fccb9d5d0a27f09b
Ubuntu <ubuntu@ip-172-31-17-23.us-west-2.compute.internal>
2013-07-13 15:35:07 +0000

    Modified agian web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSynch('index.html', function(err,data){
+fs.readFileSync('index.html', function(err,data){
 if (err) throw err;
 console.log(data);
 });
