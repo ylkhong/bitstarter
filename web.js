11f3c5a350af81eb661cd94c4866672465ae047e
Ubuntu <ubuntu@ip-172-31-17-23.us-west-2.compute.internal>
2013-07-13 15:59:40 +0000

    Modified agian web.js

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var app = express.createServer(express.logger());
 
 var buffer = new Buffer(30);
 
-var text = fs.readFileSync('index.html',);
+var text = fs.readFileSync('index.html','utf8');
 console.log(text);
  
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8');
+  response.send(buffer.toString('utf8');
 });
 
 var port = process.env.PORT || 5000;
