22f4d63eb2d1e2556b60e6aea1193791ab0a7c46
ylkhong <@yahoo.com>
2013-08-10 14:12:14 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
+#!/usr/bin/env node
+
 var express = require('express');
-
-var app = express.createServer(express.logger());
-
-var buffer = new Buffer(30);
 
 var fs = require('fs');
 
-var text = fs.readFileSync('index.html','');
+var htmlfile = "index.html";
 
-buffer = text;
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf8"));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
